869590271b4d2aed478ddfa3e7b553503eae9a51
Logan A Morrison <loanmorr@ucsc.com>
2020-04-28 20:21:06 -0700

    Documenting the constants

--- src/consts.rs
+++ src/consts.rs
@@ -1,8 +1,16 @@
+/// Euler–Mascheroni constant
 pub const EUL_GAMMA: f64 = 0.577_215_664_901_532_9_f64;
+/// Natural log of pi
 pub const LNPI: f64 = 1.144_729_885_849_400_2_f64;
+/// Natural log of f64::MAX
 pub const LN_DBL_MAX: f64 = 7.097_827_128_933_84e2_f64;
+/// Natural log of f64::MIN
 pub const LN_DBL_MIN: f64 = -7.083_964_185_322_641e2_f64;
+/// f64::EPSILON^(1/4)
 pub const ROOT4_DBL_EPS: f64 = 1.220_703_125e-4_f64;
+/// f64::EPSILON^(1/5)
 pub const ROOT5_DBL_EPS: f64 = 7.400_959_797_414_05e-4_f64;
+/// f64::EPSILON^(1/6)
 pub const ROOT6_DBL_EPS: f64 = 2.460_783_300_575_925e-3_f64;
+/// f64::EPSILON^(1/2)
 pub const SQRT_DLB_EPS: f64 = 1.490_116_119_384_765_6e-8_f64;

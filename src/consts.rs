0ad5fada9e5ba25b85b0556bfd57c7d183423e5b
Logan A Morrison <loanmorr@ucsc.com>
2020-04-30 12:14:32 -0700

    Adding yet more constants

--- src/consts.rs
+++ src/consts.rs
@@ -8,6 +8,8 @@ pub const LN_DBL_MAX: f64 = 7.097_827_128_933_84e2_f64;
 pub const LN_DBL_MIN: f64 = -7.083_964_185_322_641e2_f64;
 /// Natural log of f64::EPSILON
 pub const LN_DBL_EPS: f64 = -3.604_365_338_911_715e1_f64;
+/// f64::EPSILON^(1/3)
+pub const ROOT3_DBL_EPS: f64 = 6.055_454_452_393_342_9e-06;
 /// f64::EPSILON^(1/4)
 pub const ROOT4_DBL_EPS: f64 = 1.220_703_125e-4_f64;
 /// f64::EPSILON^(1/5)

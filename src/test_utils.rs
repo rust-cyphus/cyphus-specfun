ea8c709c1c95d20edf09cc41b3d985601b834571
Logan A Morrison <loanmorr@ucsc.com>
2020-05-01 23:41:11 -0700

    Added testing macros to match GSL

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -93,6 +93,176 @@ pub(crate) fn test_sf_check_result(r: SpecFunResult<f64>, val: f64, tol: f64) {
     );
 }
 
+#[macro_export]
+macro_rules! test_check_result_and_code {
+    ($func:ident, ($($arg:expr ),*), $val:expr, $tol:expr, $code:expr) => {
+        let r = $func($($arg),*);
+        let mut s:usize = 0;
+        let mut f: f64 = 0.0;
+        let d: f64;
+        let ival :f64 = ($val as f64);
+
+        if r.val.is_nan() ||ival.is_nan() {
+            s = if r.val.is_nan() != ival.is_nan() {
+            TestResult::InCons as usize
+        } else {
+            s
+        };
+        } else if r.val.is_infinite() || ival.is_infinite() {
+            s = if r.val.is_infinite() != ival.is_infinite() {
+            TestResult::InCons as usize
+        } else {
+            s
+        };
+        } else {
+        f = test_sf_frac_diff(ival, r.val);
+        d = (ival - r.val).abs();
+
+        if d > 2.0 * r.err {
+            s |= TestResult::InCons as usize;
+        }
+        if r.err < 0.0 {
+            s |= TestResult::ErrNeg as usize;
+        }
+        if r.err.is_infinite() {
+            s |= TestResult::ErrBad as usize;
+        }
+
+        if f > $tol {
+            s |= TestResult::TolBad as usize;
+        }
+    }
+
+    if s != 0 {
+        println!("expected: {:?}", ival);
+        println!(
+            "obtained: {:?} +/- {:?} (rel={:?})",
+            r.val,
+            r.err,
+            r.err / (r.val + r.err).abs()
+        );
+        println!("fracdiff: {:?}", f);
+        println!("tolerance: {:?}", $tol);
+        println!("call: {:?}{:?}",stringify!($func),($($arg),*));
+    }
+
+    assert!(
+        s & (TestResult::InCons as usize) == 0,
+        "value/expected not consistent within reported error"
+    );
+    assert!(
+        s & (TestResult::ErrNeg as usize) == 0,
+        "reported error negative"
+    );
+    assert!(
+        s & (TestResult::ErrBad as usize) == 0,
+        "reported error is bad"
+    );
+    assert!(
+        s & (TestResult::ErrBig as usize) == 0,
+        "reported error is much too big"
+    );
+    assert!(
+        s & (TestResult::TolBad as usize) == 0,
+        "value not within tolerance of expected value"
+    );
+    assert!(r.code == $code, "Retcodes are not equal.");
+    };
+}
+
+#[macro_export]
+macro_rules! test_check_result_and_code_e10 {
+    ($func:ident, ($($arg:expr ),*), $val:expr, $e10:expr, $tol:expr, $code:expr) => {
+        let r = $func($($arg),*);
+        let mut s:usize = 0;
+        let mut f: f64 = 0.0;
+        let d: f64;
+        let ival :f64 = ($val as f64);
+
+        if r.val.is_nan() ||ival.is_nan() {
+            s = if r.val.is_nan() != ival.is_nan() {
+            TestResult::InCons as usize
+        } else {
+            s
+        };
+        } else if r.val.is_infinite() || ival.is_infinite() {
+            s = if r.val.is_infinite() != ival.is_infinite() {
+            TestResult::InCons as usize
+        } else {
+            s
+        };
+        } else {
+        f = test_sf_frac_diff(ival, r.val);
+        d = (ival - r.val).abs();
+
+        if d > 2.0 * r.err {
+            s |= TestResult::InCons as usize;
+        }
+        if r.err < 0.0 {
+            s |= TestResult::ErrNeg as usize;
+        }
+        if r.err.is_infinite() {
+            s |= TestResult::ErrBad as usize;
+        }
+
+        if f > $tol {
+            s |= TestResult::TolBad as usize;
+        }
+    }
+
+    if s != 0 {
+        println!("expected: {:?}", ival);
+        println!(
+            "obtained: {:?} +/- {:?} (rel={:?})",
+            r.val,
+            r.err,
+            r.err / (r.val + r.err).abs()
+        );
+        println!("fracdiff: {:?}", f);
+        println!("tolerance: {:?}", $tol);
+        println!("call: {:?}{:?}",stringify!($func),($($arg),*));
+    }
+
+    assert!(
+        s & (TestResult::InCons as usize) == 0,
+        "value/expected not consistent within reported error"
+    );
+    assert!(
+        s & (TestResult::ErrNeg as usize) == 0,
+        "reported error negative"
+    );
+    assert!(
+        s & (TestResult::ErrBad as usize) == 0,
+        "reported error is bad"
+    );
+    assert!(
+        s & (TestResult::ErrBig as usize) == 0,
+        "reported error is much too big"
+    );
+    assert!(
+        s & (TestResult::TolBad as usize) == 0,
+        "value not within tolerance of expected value"
+    );
+    assert!(r.code == $code, "Retcodes are not equal.");
+
+    let s = if r.e10 != $e10 {
+        TestResult::ExpBad as usize
+    } else {
+        0
+    };
+
+    if s != 0 {
+        println!("Expected exponent: 10^{:?}", $e10);
+        println!("Obtained exponent: 10^{:?}", r.e10);
+    }
+
+    assert!(
+        s & TestResult::ExpBad as usize == 0,
+        "Exponent is incorrect."
+    );
+    };
+}
+
 pub(crate) fn test_sf_check_result_and_code(
     r: SpecFunResult<f64>,
     val: f64,

3f24fe7200e69e87e4f09179c94bdf1d72410ea2
Logan A Morrison <loanmorr@ucsc.com>
2020-05-18 20:55:10 -0700

    Adding Bessel function module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod bessel;
 pub mod cheb;
 pub(crate) mod consts;
 pub(crate) mod elementary;
@@ -8,3 +9,9 @@ pub mod result;
 pub(crate) mod test_utils;
 pub mod trig;
 pub mod zeta;
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn mytest() {}
+}
